034ae63ccca8f0222e431093d37c823c6103b9f7
MysteryCoder <srikar0811@gmail.com>
2025-07-28 20:09:34 +0530

    question submit

--- src/app/create-quiz/create-quiz.component.ts
+++ src/app/create-quiz/create-quiz.component.ts
@@ -5,9 +5,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
+
+import { Question } from '../../types';
+
 @Component({
   selector: 'app-create-quiz',
-  imports: [MatInputModule, MatFormFieldModule, FormsModule, MatButtonModule],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
   templateUrl: './create-quiz.component.html',
   styleUrl: './create-quiz.component.css',
 })
@@ -17,11 +27,13 @@ export class CreateQuizComponent {
 
   quizName = '';
   questionsAdded = 0;
+  submittedQuestions: Question[] = [];
   question = '';
   option1 = '';
   option2 = '';
   option3 = '';
   option4 = '';
+  answer = '';
 
   constructor() {
     this.activatedRoute.params.subscribe((params) => {
@@ -35,12 +47,33 @@ export class CreateQuizComponent {
       this.option1 == '' ||
       this.option2 == '' ||
       this.option3 == '' ||
-      this.option4 == ''
+      this.option4 == '' ||
+      this.answer == ''
     ) {
       this.snackBar.open('Fill all Fields', 'Dismiss', {
         duration: 1 * 1000,
       });
       return;
     }
+    let submittedQuestion: Question = {
+      question_id: 0, // Dummy ID's for DB
+      quiz_id: 0, // Dummy ID's for DB
+      question: this.question,
+      answer: this.answer,
+      option1: this.option1,
+      option2: this.option2,
+      option3: this.option3,
+      option4: this.option4,
+    };
+    this.submittedQuestions.push(submittedQuestion);
+
+    this.question = '';
+    this.option1 = '';
+    this.option2 = '';
+    this.option3 = '';
+    this.option4 = '';
+    this.answer = '';
+
+    console.log(this.submittedQuestions);
   }
 }
