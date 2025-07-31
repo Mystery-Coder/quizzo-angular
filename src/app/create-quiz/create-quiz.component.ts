95534d2569cb74716e92bc266207a0e92a337552
MysteryCoder <srikar0811@gmail.com>
2025-07-31 14:00:05 +0530

    Create Quiz post

--- src/app/create-quiz/create-quiz.component.ts
+++ src/app/create-quiz/create-quiz.component.ts
@@ -7,9 +7,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSelectModule } from '@angular/material/select';
 import { NgIf, NgFor } from '@angular/common';
-import { Question } from '../../types';
+import { Question, Quiz, QuizDataResponse } from '../../types';
 import { MatIcon } from '@angular/material/icon';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { catchError, of } from 'rxjs';
 @Component({
   selector: 'app-create-quiz',
   imports: [
@@ -28,6 +38,7 @@ import { MatIcon } from '@angular/material/icon';
 export class CreateQuizComponent {
   private activatedRoute = inject(ActivatedRoute);
   private snackBar = inject(MatSnackBar);
+  private dialog = inject(MatDialog);
 
   quizName = '';
   questionsAdded = 0;
@@ -79,5 +90,103 @@ export class CreateQuizComponent {
     this.answer = '';
 
     console.log(this.submittedQuestions);
+  }
+
+  submitQuiz() {
+    let quizToSubmit: Quiz = {
+      quiz_id: 0,
+      quiz_name: this.quizName,
+      submitted_at: new Date().toISOString(),
+      submitted_by: '',
+    };
+
+    if (this.submittedQuestions.length < 3) {
+      this.snackBar.open('Minimum 3 Questions', 'OK', {
+        duration: 0.5 * 1000,
+      });
+      return;
+    }
+
+    let quizDataPost: QuizDataResponse = {
+      quiz: quizToSubmit,
+      questions: this.submittedQuestions,
+    };
+
+    this.dialog
+      .open(ConfirmationDialog, {
+        data: quizDataPost,
+        disableClose: true,
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        if (result) {
+          this.snackBar.open('Quiz Submitted Successfully!', 'OK', {
+            duration: 1000,
+          });
+        }
+      });
+  }
+}
+
+@Component({
+  selector: 'confirmation-dialog',
+  standalone: true,
+  template: `<h2 mat-dialog-title>Submit Quiz</h2>
+    <mat-dialog-content
+      ><mat-form-field>
+        <mat-label>Enter Submitter Name</mat-label>
+        <input [(ngModel)]="submitterName" matInput /> </mat-form-field
+    ></mat-dialog-content>
+    <mat-dialog-actions>
+      <button mat-button mat-dialog-close>Cancel</button>
+      <button mat-button (click)="submit()">Submit</button>
+    </mat-dialog-actions>`,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+})
+export class ConfirmationDialog {
+  submitterName = '';
+  private data = inject<QuizDataResponse>(MAT_DIALOG_DATA);
+  private http = inject(HttpClient);
+  private snackBar = inject(MatSnackBar);
+  private dialogRef = inject(MatDialogRef<ConfirmationDialog>);
+
+  submit() {
+    if (this.submitterName == '') {
+      this.snackBar.open('Name Required', 'OK', { duration: 1000 });
+      return;
+    }
+    this.data.quiz.submitted_by = this.submitterName;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    this.http
+      .post('http://localhost:8000/new_quiz', this.data, { headers })
+      .pipe(
+        catchError((err) => {
+          if (err.status == 400) {
+            console.error('Quiz Name Taken');
+            this.snackBar.open('Quiz Name Taken!', 'OK', {
+              duration: 1 * 1000,
+            });
+          } else {
+            console.error('Server Error');
+          }
+          return of(null);
+        })
+      )
+      .subscribe((data) => {
+        if (!data) {
+          console.log('Could Not submit');
+        } else {
+          this.dialogRef.close(true);
+        }
+      });
   }
 }
